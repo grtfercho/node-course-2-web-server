a143801e20cb40095f90828b5c1c8f2d7cd1d479
fernando lopez <fernandolopez@MacBook-Pro.local>
2018-09-06 14:10:53 -0400

    Initial commit for Express project

---
+++ server.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine','hbs');
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear',() => {
+            return 'Año del señor: ' + new Date().getFullYear();
+ 		});
+
+hbs.registerHelper('screamIt',(text) => {
+            return text.toUpperCase();
+ 		});
+
+// To create routes in Express just have a list of multiple app.get entries in the code
+// These could be reorganized on external files
+
+//log all the requests and then move to the next Middleware piece.
+app.use((req, res, next)=>{
+    var reqTime=  new Date().toString();
+    var log = `Request Made: ${reqTime} ${req.method} ${req.url} \n`;
+    console.log(log);
+    fs.appendFileSync('server-log.txt',log);
+    next();
+});
+//maintenance page using middleware
+app.use((req, res, next)=>{
+    res.render('maintenance.hbs');
+    // next();
+});
+
+
+
+app.get('/',(requestToTheServer,responseFromTheServer) => {
+        // responseFromTheServer.send('helllo from Express-Node');
+        responseFromTheServer.render('home.hbs',{
+            pageTitle:"You are home",
+            welcomeMessage : [{'es':'Bienvenido al website',
+        en:'Welcome to the wbesite'},2,'a']
+
+        })
+
+ 		});
+app.get('/about',(req, res) => {
+        //res.send('This is the about us page'); we stop sending a simple response and replace it with a new rendered page.
+        res.render('about.hbs',{
+            pageTitle:"This is the About Page"
+        });
+ 	});
+app.get('/bad',(req, res) => {
+        res.send({errorMessage:'The url requested doesnt exist'});
+ 		});
+
+app.listen(3000,() => {
+        console.log('Server running on port 3000');
+ 		});
